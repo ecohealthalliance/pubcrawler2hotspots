caf016b2faa3373da79f5d43e1ac951810b7a0d6
Toph Allen <toph.allen@gmail.com>
2016-10-03 21:42:40 -0400

    Script to read in all the CSV files. It's satisfyingly fast!

--- inst/scratch/pubcrawler2hotspots.R
+++ inst/scratch/pubcrawler2hotspots.R
@@ -1,1 +1,31 @@
 library(raster)
+library(readr)
+library(purrr)
+library(dplyr)
+library(foreach)
+library(doParallel)
+
+# Functions
+bind_csvs <- function(datadir, col_types = "icddcdic") {
+  files <- list.files(datadir, recursive = TRUE, full.names = TRUE)
+  bound <- files %>%
+    map(read_csv, col_types = col_types) %>%
+    reduce(rbind)
+  return(bound)
+}
+
+# Script
+registerDoParallel()
+
+
+datadir <- "../pubcrawler/export/2016-10-03_14-26-52"
+list.files(datadir)
+datadirs <- file.path(datadir, list.files(datadir))
+
+datadirs
+
+locations <- foreach(d = datadirs, .combine = rbind) %dopar% {
+  bind_csvs(d)
+}
+
+save(locations, file = "data/locations.RData")
